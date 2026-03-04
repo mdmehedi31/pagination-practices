df0fd8b0188cac128375bdc92b66843f1df5e004
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-03-04 22:09:53 +0600

    initial commit

---
+++ src/test/java/com/practices/PaginationPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
